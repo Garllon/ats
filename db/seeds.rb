922ac1441757b7a9520629aaedcd0d10b64ed960
Garllon <bennet.palluthe@protonmail.com>
2025-06-13 21:45:48 +0200

    add more logic to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,25 +8,83 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-puts 'create Jobs'
-jobs = [
-  { title: 'Software Engineer',
-    description: 'We are looking for a skilled software engineer to join our team.' \
-                 'The ideal candidate will have experience in Ruby on Rails and a passion' \
-                 'for building scalable applications.' },
-  { title: 'Product Manager',
-    description: 'We are seeking a product manager to lead our product development team.' \
-                 'The ideal candidate will have experience in Agile methodologies and a' \
-                 'strong understanding of user experience.' },
-  { title: 'Data Scientist',
-    description: 'We are looking for a data scientist to analyze large datasets and provide' \
-                 'insights to drive business decisions. The ideal candidate will have experience' \
-                 'in machine learning and statistical analysis.' }]
+def create_activated_jobs
+  puts 'Creating Activated Jobs'
 
-jobs.each do |job|
-  Job.find_or_create_by!(title: job[:title]) do |j|
-    j.description = job[:description]
+  raw_jobs = [
+    { title: 'Software Engineer',
+      description: 'We are looking for a skilled software engineer to join our team.' \
+        'The ideal candidate will have experience in Ruby on Rails and a passion' \
+        'for building scalable applications.' },
+    { title: 'Product Manager',
+      description: 'We are seeking a product manager to lead our product development team.' \
+        'The ideal candidate will have experience in Agile methodologies and a' \
+        'strong understanding of user experience.' },
+    { title: 'Data Scientist',
+      description: 'We are looking for a data scientist to analyze large datasets and provide' \
+        'insights to drive business decisions. The ideal candidate will have experience' \
+        'in machine learning and statistical analysis.' }]
+
+  raw_jobs.each do |raw_job|
+    job = Job.find_or_create_by!(title: raw_job[:title]) do |j|
+      j.description = raw_job[:description]
+    end
+
+    Job::Event::Activated.create!(
+      job: job,
+      created_at: 3.days.ago
+    )
   end
+
+  puts 'Jobs created successfully!'
 end
 
-puts 'Jobs created successfully!'
+def create_applications_for_software_engineer
+  job = Job.find_by(title: 'Software Engineer')
+
+  # Applicatant 1
+  applicant_1 = Application.create!(
+    job: job,
+    candidate_name: "Darth Vader")
+
+  Application::Event::Interview.create!(
+    application: applicant_1,
+    metadata: {
+      interview_date: 2.days.ago },
+    created_at: 2.days.ago
+  )
+  Application::Event::Hired.create!(application: applicant_1,
+                                    metadata: { hire_date: 1.day.ago },
+                                    created_at: 1.day.ago)
+
+  # Applicant 2
+  applicant_2 = Application.create!(
+    job: job,
+    candidate_name: "Master Yoda")
+
+  Application::Event::Rejected.create!(
+    application: applicant_2,
+    created_at: 2.days.ago
+  )
+
+  # Applicant 3
+  applicant_3 = Application.create!(
+    job: job,
+    candidate_name: "Grogu")
+
+  Application::Event::Note.create!(
+    application: applicant_3,
+    metadata: {
+      content: "Good potential, consider for another role."
+    })
+
+  job.events.create!(type: 'Job::Event::Deactivated')
+end
+
+Job::Event.delete_all
+Application::Event.delete_all
+Application.delete_all
+Job.delete_all
+
+create_activated_jobs
+create_applications_for_software_engineer
